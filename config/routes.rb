13282795525d6d0d67e53c53049bbfd62dc46831
Nzakou <nzakou.cedric@protonmail.com>
2020-03-27 22:50:00 +0000

    Set up devise add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :login
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'devise/sessions#new'
 end
