30e16db3135265b9f2eac6b3f1c84288dc546479
Nzakou <nzakou.cedric@protonmail.com>
2020-03-30 05:34:42 +0100

    format

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
-Airbrake.configure do |c|
-  c.ignore_environments = %w(development test)
-  c.project_id = ENV["AIRBRAKE_PROJECT_ID"]
-  c.project_key = ENV["AIRBRAKE_PROJECT_KEY"]
-end
+#Airbrake.configure do |c|
+#  c.ignore_environments = %w(development test)
+#  c.project_id = ENV["AIRBRAKE_PROJECT_ID"]
+#  c.project_key = ENV["AIRBRAKE_PROJECT_KEY"]
+#end
 
